74ca9e2c5719af485d14380005e831f852326f78
mei kuroda <yayiyayi0327@gmail.com>
2026-09-04 19:18:57 +0900

    feat: ジャンル登録のバリデーション実装

---
+++ app/Http/Requests/Web/GenreRegisterRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Web;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class GenreRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:50', Rule::unique('genres')],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'ジャンル名を入力してください',
+            'name.string' => 'ジャンル名は文字列で入力してください',
+            'name.max' => 'ジャンル名は50文字以内で入力してください',
+            'name.unique' => 'このジャンル名は既に他のジャンルに登録されています',
+        ];
+    }
+}
