3f90366ca2c3cbaf2816a1234f0a2bf0e551bfc4
mei kuroda <yayiyayi0327@gmail.com>
2026-09-13 20:32:02 +0900

    feat: ランキング画面の実装

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+
 class RankingController extends Controller
 {
     public function index()
     {
-        return view('ranking.index');
+        $rankedBooks = Book::has('reviews')
+            ->withAvg('reviews', 'rating')
+            ->withCount('reviews')
+            ->orderBy('reviews_avg_rating', 'desc')
+            ->orderBy('reviews_count', 'desc')
+            ->take(10)
+            ->get();
+
+        return view('ranking.index', compact('rankedBooks'));
     }
 }
