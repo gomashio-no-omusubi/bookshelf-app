49165d3db537246bd38ea7a88e0c45da45a83e94
mei kuroda <yayiyayi0327@gmail.com>
2026-09-15 11:44:35 +0900

    feat: api.phpに書籍CRUD（一覧・登録・詳細・更新・削除）のルートを定義

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::get('/books', [BookController::class, 'index'])->name('api.v1.books.index');
+    Route::post('/books', [BookController::class, 'store'])->name('api.v1.books.store');
+    Route::get('/books/{book}', [BookController::class, 'show'])->name('api.v1.books.show');
+    Route::put('/books/{book}', [BookController::class, 'update'])->name('api.v1.books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('api.v1.books.destroy');
 });
